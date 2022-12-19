69fae1ba484830ef0f853f768c966b21d95921ec
Anton <shvetsov.a.fist@gmail.com>
2022-12-20 01:06:48 +0600

    Добавил новые функции
    В файле `model.py` добавил новые функции:
    - `get_duration()`` - возвращает `timedelta`
    - `format_duration()` - приводит `timedelta` к часам, минутам и секундам
    - `is_visit_long` - высчитывает длительность визита, возвращает True или False

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,40 @@
 from django.db import models
+import django.utils.timezone
+import datetime
+
+
+def get_duration(visit):
+    entered = django.utils.timezone.localtime(visit.entered_at)
+    leaved = django.utils.timezone.localtime(visit.leaved_at)
+    duration = leaved - entered
+    return duration.total_seconds()
+
+
+def format_duration(duration):
+    seconds_in_hour = 3600
+    seconds_in_minute = 60
+    seconds_in_day = 86400
+    hours_in_day = 24
+    days = duration // (hours_in_day * seconds_in_hour)
+    hours = (duration - (days * seconds_in_day)) // seconds_in_hour
+    minutes = (
+                duration - (days * seconds_in_day + hours * seconds_in_hour)
+              ) // seconds_in_minute
+    seconds = (
+            duration - (days * seconds_in_day + hours * seconds_in_hour +
+                        minutes * seconds_in_minute)
+    )
+    total_time_in = datetime.time(int(hours), int(minutes), int(seconds))
+    return f'{total_time_in.strftime("%H:%M:%S")}'
+
+
+def is_visit_long(visit, minutes=60):
+    seconds_in_minute = 60
+    total_second = get_duration(visit)
+    how_long_inside = total_second // seconds_in_minute
+    if how_long_inside > minutes:
+        return True
+    return False
 
 
 class Passcard(models.Model):
