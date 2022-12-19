022d438a9ac3e60765a2aed82ad0879491a3f764
Anton <shvetsov.a.fist@gmail.com>
2022-12-20 01:12:36 +0600

    Обновил сайт
    Добавил на сайт информацию о людях, которые сейчас находятся в хранилище и длительность их пребывания.

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,23 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from .models import format_duration, get_duration
+from django.utils.timezone import localtime
 
 
 def storage_information_view(request):
-    # Программируем здесь
-
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
+    who_inside_storage = Visit.objects.filter(leaved_at=None)
+    for visit in who_inside_storage:
+        duration = get_duration(visit)
+        how_long_in_storage = format_duration(duration)
+        enteread_at = localtime(visit.entered_at)
+        non_closed_visits = [
+            {
+                'who_entered': visit.passcard,
+                'entered_at': enteread_at,
+                'duration': how_long_in_storage,
+            }
+        ]
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
