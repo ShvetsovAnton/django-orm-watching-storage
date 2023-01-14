5fd885c7ad6fec73845908a10424570560cbed94
Anton <56098143+ShvetsovAnton@users.noreply.github.com>
2023-01-14 10:31:19 +0400

    Спрятал чувствительные переменные

--- project/settings.py
+++ project/settings.py
@@ -1,21 +1,34 @@
 import os
 
+from environs import Env
+
+
+env = Env()
+env.read_env()
+django_db_engine = env.str("DJANGO_DB_ENGINE")
+django_db_host = env.str("DJANGO_DB_HOST")
+django_db_port = env.str("DJANGO_DB_PORT")
+django_db_name = env.str("DJANGO_DB_NAME")
+django_db_user = env.str("DJANGO_DB_USER")
+django_db_password = env.str("DJANGO_DB_PASSWORD")
+django_db_secretkey = env.str("DJANGO_DB_SECRET_KEY")
+django_db_debug = env.bool("DJANGO_DB_DEBUG")
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'ENGINE': django_db_engine,
+        'HOST': django_db_host,
+        'PORT': django_db_port,
+        'NAME': django_db_name,
+        'USER': django_db_user,
+        'PASSWORD': django_db_password,
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = django_db_secretkey
 
-DEBUG = True
+DEBUG = django_db_debug
 
 ROOT_URLCONF = 'project.urls'
 
